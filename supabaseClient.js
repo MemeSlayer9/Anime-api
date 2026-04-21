11c85441ea86e3631fd507265700671f805c5795
hevavi12 <kentamarado21@gmail.com>
2026-04-22 03:01:22 +0800

    remove sensitive files

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  'https://elsqusmvaeyrcjqbqqfz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsc3F1c212YWV5cmNqcWJxcWZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1MTczNTEsImV4cCI6MjA0NTA5MzM1MX0.H3HV2HHIarOjmfe5Bmg0h9GQ07DG3EHkr_jh0XsK6EQ'
-);
-
-module.exports = supabase;
