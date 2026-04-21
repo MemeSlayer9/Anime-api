79b28363283aaa190790ebe060e1a88397657aaf
hevavi12 <kentamarado21@gmail.com>
2026-04-22 03:23:00 +0800

    upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  'https://elsqusmvaeyrcjqbqqfz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsc3F1c212YWV5cmNqcWJxcWZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1MTczNTEsImV4cCI6MjA0NTA5MzM1MX0.H3HV2HHIarOjmfe5Bmg0h9GQ07DG3EHkr_jh0XsK6EQ'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 module.exports = supabase;
